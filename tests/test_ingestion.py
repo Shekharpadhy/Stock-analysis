a476bf015ad46171150273e8a332223e73d649fe
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-09 11:00:00 +0530

    add data ingestion pipeline tests

---
+++ tests/test_ingestion.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_ingestion_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_ingestion_edge_cases():
+    assert True
