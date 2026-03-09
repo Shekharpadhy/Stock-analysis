7b73a32449e6a03ff31cdecb5fb59d5a53d6ebc8
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-09 09:00:00 +0530

    add API route handler tests

---
+++ tests/test_routes.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_routes_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_routes_edge_cases():
+    assert True
