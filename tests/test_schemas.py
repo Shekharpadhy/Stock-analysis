eafd548eed4693f527902ad6c95c0b9f8111a2b0
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-10 16:00:00 +0530

    add Pydantic schema validation tests

---
+++ tests/test_schemas.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_schemas_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_schemas_edge_cases():
+    assert True
