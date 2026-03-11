99567b03e582266f6b819d137f1e17dfe5210efb
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-11 12:30:00 +0530

    add response formatter tests

---
+++ tests/test_formatters.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_formatters_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_formatters_edge_cases():
+    assert True
