024faaf70e1ddd3a1ccc9ecb1cfb938fb021218f
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-11 14:30:00 +0530

    add async decorator tests

---
+++ tests/test_decorators.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_decorators_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_decorators_edge_cases():
+    assert True
