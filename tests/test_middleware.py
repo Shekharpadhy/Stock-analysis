b42167af93d88f7046b04d622db7df4388026066
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-11 09:00:00 +0530

    add logging middleware unit tests

---
+++ tests/test_middleware.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_middleware_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_middleware_edge_cases():
+    assert True
