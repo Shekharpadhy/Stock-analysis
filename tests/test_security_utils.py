4db86d8d09ac6955739bdf6af1235b0994201181
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-03-12 12:00:00 +0530

    add security utility tests

---
+++ tests/test_security_utils.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_security_utils_basic():
+    assert True  # TODO: add assertions
+
+@pytest.mark.asyncio
+async def test_security_utils_edge_cases():
+    assert True
