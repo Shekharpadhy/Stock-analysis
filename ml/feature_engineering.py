dd306032ef599252e2626fcc948975c04059bea5
Shekhar Padhy <padhyshekhar05@gmail.com>
2026-04-28 13:42:00 +0530

    feat(ml): add feature engineering pipeline for credit features

---
+++ ml/feature_engineering.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def engineer_credit_features(df: pd.DataFrame) -> pd.DataFrame:
+    df['debt_to_equity'] = df['total_debt'] / df['equity']
+    df['interest_coverage'] = df['ebit'] / df['interest_expense']
+    return df
+
